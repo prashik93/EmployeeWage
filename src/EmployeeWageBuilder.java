dc9399fd06a2e3dd16e0b21255673a27b19e31f3
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-17 14:21:52 +0530

    MODIFIED : Code to calculate daily employee wage.

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,11 +1,15 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int empWagePerDay = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+            empWagePerDay = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee is Present and his wage per day is : " + empWagePerDay);
         } else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent and his wage per day is : " + empWagePerDay);
         }
     }
 }
