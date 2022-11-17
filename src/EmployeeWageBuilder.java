9eaaaa25e7b6f948f4e3c063a1ef3273ffba09bc
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-17 15:54:27 +0530

    MODIFIED : Code to calculate wages for a month.

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,20 +1,40 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
+        //CONSTANTS
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOUR = 8;
         int PART_TIMR_HOUR = 4;
-        int empWagePerDay = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        switch ((int) empCheck) {
-            case 1 -> {
-                empWagePerDay = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Employee is full time present and his wage per day is : " + empWagePerDay);
+        int NUMBER_OF_WORKING_DAYS = 20;
+
+        //VARIABLES
+        int fullDayPresent = 0;
+        int partTimePresent = 0;
+        int absent = 0;
+        int totalWorkingHours = 0;
+        int wagesForAMonth = 0;
+
+        for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+            int workingHours = 0;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case 1 -> {
+                    workingHours = (workingHours + FULL_DAY_HOUR);
+                    fullDayPresent++;
+                }
+                case 2 -> {
+                    workingHours = (workingHours + PART_TIMR_HOUR);
+                    partTimePresent++;
+                }
+                default -> {
+                    workingHours = 0;
+                    absent++;
+                }
             }
-            case 2 -> {
-                empWagePerDay = WAGE_PER_HOUR * PART_TIMR_HOUR;
-                System.out.println("Employee is part time present and his wage per day is : " + empWagePerDay);
-            }
-            default -> System.out.println("Employee is Absent and his wage per day is : " + empWagePerDay);
+            totalWorkingHours = totalWorkingHours + workingHours;
         }
+        System.out.println("Full Day Present : " + fullDayPresent + " Part Time Present : " + partTimePresent + " Absent : " + absent);
+        System.out.println("Working Hours : " + totalWorkingHours);
+        wagesForAMonth = (totalWorkingHours * WAGE_PER_HOUR);
+        System.out.println("Wages for a month is : " + wagesForAMonth);
     }
 }
