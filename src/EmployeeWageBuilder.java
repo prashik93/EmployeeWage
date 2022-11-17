55029c948131b4950de2822fa899ed41d2d223fc
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-17 14:35:34 +0530

    MODIFIED : Code to change if-else statement as switch-case statement.

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,20 +1,20 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOUR = 8;
         int PART_TIMR_HOUR = 4;
         int empWagePerDay = 0;
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            empWagePerDay = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee is full time present and his wage per day is : " + empWagePerDay);
-        } else if (empCheck == IS_PART_TIME) {
-            empWagePerDay = WAGE_PER_HOUR * PART_TIMR_HOUR;
-            System.out.println("Employee is part time present and his wage per day is : " + empWagePerDay);
-        } else {
-            System.out.println("Employee is Absent and his wage per day is : " + empWagePerDay);
+        switch ((int) empCheck) {
+            case 1 -> {
+                empWagePerDay = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee is full time present and his wage per day is : " + empWagePerDay);
+            }
+            case 2 -> {
+                empWagePerDay = WAGE_PER_HOUR * PART_TIMR_HOUR;
+                System.out.println("Employee is part time present and his wage per day is : " + empWagePerDay);
+            }
+            default -> System.out.println("Employee is Absent and his wage per day is : " + empWagePerDay);
         }
     }
 }
