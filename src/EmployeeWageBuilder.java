9a32f28df8a80cc4f01e8588122b258a6e800bcb
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-18 10:52:14 +0530

    REFACTORED : Code use static final constants and while loop.

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,41 +1,30 @@
 public class EmployeeWageBuilder {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOUR = 8;
+    public static final int PART_TIME_HOUR = 4;
+    public static final int NUMBER_OF_WORKING_DAYS = 20;
+    public static final int MAX_WORKING_HOURS = 100;
     public static void main(String[] args) {
-        //CONSTANTS
-        int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        int PART_TIMR_HOUR = 4;
-        int NUMBER_OF_WORKING_DAYS = 20;
-        int MAX_WORKING_HOURS = 100;
-
-        //VARIABLES
-        int fullDayPresent = 0;
-        int partTimePresent = 0;
-        int absent = 0;
         int totalWorkingHours = 0;
-        int wagesForAMonth = 0;
-
-        for (int day = 0; ((day < NUMBER_OF_WORKING_DAYS) && (totalWorkingHours < MAX_WORKING_HOURS)); day++) {
+        int day = 0;
+        while ((day < NUMBER_OF_WORKING_DAYS) && (totalWorkingHours <= MAX_WORKING_HOURS)) {
+            day++;
             int workingHours = 0;
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
-                case 1 -> {
+                case IS_FULL_TIME ->
                     workingHours = (workingHours + FULL_DAY_HOUR);
-                    fullDayPresent++;
-                }
-                case 2 -> {
-                    workingHours = (workingHours + PART_TIMR_HOUR);
-                    partTimePresent++;
-                }
-                default -> {
+                case IS_PART_TIME ->
+                    workingHours = (workingHours + PART_TIME_HOUR);
+                default ->
                     workingHours = 0;
-                    absent++;
-                }
             }
             totalWorkingHours = totalWorkingHours + workingHours;
+            System.out.println("Total Working Days are : " + day + " And Total Working Hours are : " + totalWorkingHours);
         }
-        System.out.println("Full Day Present : " + fullDayPresent + " Part Time Present : " + partTimePresent + " Absent : " + absent);
-        System.out.println("Working Hours : " + totalWorkingHours);
-        wagesForAMonth = (totalWorkingHours * WAGE_PER_HOUR);
+        int wagesForAMonth = (totalWorkingHours * WAGE_PER_HOUR);
         System.out.println("Wages for a month is : " + wagesForAMonth);
     }
 }
